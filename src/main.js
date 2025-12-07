fd984d7ea3a4db67ebbfd099ac25715b20fee54e
callmerichie <bertoli.r@live.it>
2025-12-07 16:10:00 +0100

    html/js working

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@
 * */
 
 const products = [];
+let AUTO_INCREMENT = 0;
+
 
 class Product {
       constructor(name, quantity, category, price) {
+        this.idProduct = AUTO_INCREMENT
         this.name = name;
         this.quantity = quantity;
         this.category = category;
@@ -39,30 +42,33 @@ function insertProduct(event) {
   event.preventDefault(); //don't reload page
 
   console.log("Validation");
-  let form = document.forms['insertForm'] // get the data from the form
-  let counter = String(products.length + 1); // for the id of the row
+  let form = document.forms['insertForm']; // get the data from the form
   console.log(form);
 
   if (validateForm(form) === true) {
-    console.log("User Insert Values, Insert ROW")
+    console.log("User Insert Values, Insert ROW");
+
+    AUTO_INCREMENT += 1
     product = new Product(form.elements['fnameInsert'].value,
       form.elements['quantityInsert'].value,
       form.elements['categoryInsert'].value,
       form.elements['priceInsert'].value);
 
+    let idProduct = product.idProduct;
+
     //add the row to the table
     document.getElementById("tbody").innerHTML +=
-      "<tr id='rownumber" + counter + "'><td>" + product['name'] + "</td>" +
+      "<tr id='rowNumber" + idProduct + "'><td>" + product['name'] + "</td>" +
       "<td>" + product['quantity'] + "</td>" +
       "<td>" + product['category'] + "</td>" +
       "<td>" + product['price'] + "</td>" +
       "<td>" + product['date'] + "</td>" +
-      "<td><button id='updateRow"+ counter +"' onclick='updateProduct(\"" + counter + "\")'>UPDATE ROW</button></td>" +
-      "<td><button id='deleteRow"+ counter +"' onclick='deleteProduct(\"" + counter+ "\")'>DELETE ROW</button></tr>";
+      "<td><button id='updateRow"+ idProduct +"' onclick='updateProduct(\"" + idProduct + "\")'>UPDATE ROW</button></td>" +
+      "<td><button id='deleteRow"+ idProduct +"' onclick='deleteProduct(\"" + idProduct + "\")'>DELETE ROW</button></tr>";
 
       products.push(product); // add the product, keeps track for deleting
-      console.log(products);
-      form.reset()
+      //console.log(products);
+      form.reset();
 
   } else {
     alert("Errore nell'inserimento dati");
@@ -70,12 +76,29 @@ function insertProduct(event) {
   }
 }
 
-function updateProduct(rowNumber) {
-  console.log("User choose to update value: " + rowNumber);
+function updateProduct(idProduct) {
+  console.log("User choose to update value: " + idProduct);
 }
 
-function deleteProduct(rowNumber) {
-  console.log("User choose to delete value" + rowNumber);
+function deleteProduct(idProduct) {
+  console.log("User choose to delete value: " + idProduct);
+
+  let rowId = "rowNumber" + idProduct;
+  let row = document.getElementById(rowId);
+  row.remove() //remove line
+
+  // find product to delete
+  for(let i=0; i<products.length; i++) {
+
+    console.log(products[i]);
+
+    if (products[i]['idProduct'] === Number(idProduct)) {
+      products.splice(i, 1);
+      break;
+    }
+  }
+
+  console.log(products);
 }
 
 function validateForm(formElements) {
